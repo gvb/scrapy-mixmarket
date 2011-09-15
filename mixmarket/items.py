3adfd631e6bf4db86bc37e2d3ba0dbbca5306ca5
Gerald Van Baren <gerald.vanbaren@cri.us.com>
2011-09-15 08:43:36 -0400

    Extracting company info

--- mixmarket/items.py
+++ mixmarket/items.py
@@ -6,6 +6,27 @@
 from scrapy.item import Item, Field
 
 class MixmarketItem(Item):
-    # define the fields for your item here like:
-    # name = Field()
-    pass
+    details_link = Field()	# company details page on mixmarket
+    country = Field()
+    institution = Field()
+    date = Field()		# of what? update?
+    rating = Field()		# "diamonds"
+    portfolio = Field()		# Gross loan portfolio
+    borrowers = Field()		# Number of active borrowers
+#    deposits = Field();
+#    num_depositors = Field();
+#    total_assets = Field();
+#    mission = Field();
+#    address = Field();		# Multi-line represented as an array
+#    phone = Field();
+#    email = Field();
+#    website = Field();
+#    fundingsrc = Field();
+#    products_svcs = Field();
+#    looking_for = Field();
+#    percent_ops_mf = Field();
+#    established = Field();
+#    fye = Field();
+#    legal_status = Field();
+#    regulated = Field();
+#    partnerships = Field();	# Array of [organization, relationship]
